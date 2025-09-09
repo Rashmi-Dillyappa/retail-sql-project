3162cbb1dd95a0eb6a936865891248a256061323
Rashmi Dillyappa <rashmi.dillyappa@gmail.com>
2025-09-09 17:43:25 +1000

    STUFF and For XML PATH).sql

---
+++ STUFF and For XML PATH).sql
@@ -0,0 +1,64 @@
+SELECT
+ STUFF (
+         (
+		   SELECT 
+		     ','+ CAST(CAST(LineTotal AS money) AS varchar)
+			 FROM adventureworks2022.sales.salesorderdetail b
+			 WHERE b.SalesOrderID = 43659
+			 FOR XML PATH ('')
+			 ),
+			 1,1,'')
+
+How STUFF() works in SQL:
+--------------------------------------------------------------------
+STUFF (character_expression, start, length, replaceWith)
+eg: SELECT STUFF('HelloWorld', 6, 5, 'SQL')
+
+It deletes characters from a string and replaces them with something else.
+How FOR XML PATH('') works :
+----------------------------------------------------------------------------
+Normally FOR XML PATH converts rows into XML. 
+But when you pass an empty string '', it just concatenates rows as plain text.
+
+Example:
+
+SELECT name + ',' 
+FROM sys.objects
+FOR XML PATH('')
+
+Returns all object names concatenated with commas.
+(Instead of multiple rows, you get one big string).
+So, FOR XML PATH('') is a string concatenation trick in SQL.
+  
+
+Inner SELECT
+
+SELECT ','+ CAST(CAST(LineTotal AS money) AS varchar)
+FROM sales.salesorderdetail b
+WHERE b.SalesOrderID = 43659
+FOR XML PATH('')
+Fetches all LineTotal values for order 43659.
+
+Adds a comma before each value.
+
+FOR XML PATH('') glues them into a single string.
+👉 Example result:
+,150.00,200.00,300.00
+
+Outer STUFF
+
+STUFF(big_string, 1, 1, '')
+Removes the first character (the extra comma).
+
+Final result:
+150.00,200.00,300.00
+
+In simple words:
+  This query fetches all LineTotal values for order 43659, concatenates them into a comma-separated string, and removes the first extra comma
+
+
+
+
+
+
+
